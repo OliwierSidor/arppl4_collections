2fc51b3619ff83dedf0d0b7ba28a3937a45d000b
Oliwier <oliwiersidor@gmail.com>
2022-05-08 16:03:23 +0200

    08.05 commit kolejny

--- src/main/java/pl/sda/arp4/collections/MainGenerator1.java
+++ src/main/java/pl/sda/arp4/collections/MainGenerator1.java
@@ -1,15 +1,17 @@
 package pl.sda.arp4.collections;
 
+import java.util.Arrays;
 import java.util.Random;
 
-public class MainGenerator {
+public class MainGenerator1 {
     public static void main(String[] args) {
 
 
         int[] tablica = new int[100];
         for (int i = 0; i < tablica.length; i++) {
-            int liczba = new Random().nextInt(50) ;
-            System.out.print(liczba + " ");
+            int liczba = new Random().nextInt(50);
+            tablica[i] = liczba;
         }
+        System.out.print(Arrays.toString(tablica));
     }
 }
--- src/main/java/pl/sda/arp4/collections/MainZad1Tablica.java
+++ src/main/java/pl/sda/arp4/collections/MainZad1Tablica.java
@@ -1,2 +1,18 @@
-package pl.sda.arp4.collections;public class MainZad1Tablica {
+package pl.sda.arp4.collections;
+
+import java.util.Random;
+
+public class MainZad1Tablica {
+    public static void main(String[] args) {
+        int[] tablica = new int[10];
+
+        for (int i = 0; i < tablica.length; i++) {
+            int liczby = new Random().nextInt(21) - 10;
+
+            tablica[i] = liczby;
+        }
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.print(tablica[i] + " | ");
+        }
+    }
 }
